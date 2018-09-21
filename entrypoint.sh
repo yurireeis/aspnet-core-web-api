5cf3eaa0e29d9724220dd83656e8ca096eaee43a
Yuri Reis <yuri.reis@kch.com.br>
2018-09-21 11:42:14 -0300

    Set specific docker launchSettings.json config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 VERSION=$(dotnet --list-sdks |grep -oP '(\d+)\.(\d+)\.(\d+)')
 dotnet new globaljson --sdk-version "${VERSION}" --force
 dotnet build -o "./${DLL_PATH}/" -r linux-x64
-dotnet watch run "./${DLL_PATH}/AspNetWebApi.dll"
+dotnet watch run --launch-profile "Docker" "./${DLL_PATH}/AspNetWebApi.dll"
 # tail -f /dev/null # for debugging
